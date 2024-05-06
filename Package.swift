d32e09be50c9a2cb651cd749b6e0a73d1e1979f6
Mehul Parmar <mparmar.mailbox@gmail.com>
2024-05-06 11:29:41 +0530

    Updated Package Configuration

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version: 5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,20 +6,14 @@ import PackageDescription
 let package = Package(
     name: "Reachability",
     products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "Reachability",
             targets: ["Reachability"]),
     ],
     targets: [
-        .target(
-            name: "Reachability",
-            dependencies: [],
-            path: "Sources",
-            resources: [.copy("PrivacyInfo.xcprivacy")]
-        ),
-        .testTarget(
-            name: "ReachabilityTests",
-            dependencies: ["Reachability"],
-            path: "Tests"),
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(name: "Reachability")
     ]
 )
